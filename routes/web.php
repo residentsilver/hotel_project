b31fadd15d5f6fdd697548b5582ce36e292e0dda
residentsilve <testoftest.fornara@gmail.com>
2023-12-07 13:10:38 +0900

    微修正

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GuestController;
 use App\Http\Controllers\MasterController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\ReservationController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
